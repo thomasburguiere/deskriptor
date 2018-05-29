908189d8145ef5fcd0e5551cd29bff22d96b7098
Thomas Burguiere <thomas.burguiere@gmail.com>
2018-05-29 07:00:10 +0200

    more coverage

--- src/test/java/ch/burg/deskriptor/model/MeasureTest.java
+++ src/test/java/ch/burg/deskriptor/model/MeasureTest.java
@@ -11,8 +11,8 @@ public class MeasureTest {
         final Measure measure1 = Measure.withMin(0).andMax(10);
         final Measure measure2 = Measure.withMin(5).andMax(15);
 
-        final double commonRatio = measure1.commonRatioWithAnotherMeasure(measure2);
-        assertThat(commonRatio).isBetween(0.333, 0.334);
+        assertThat(measure1.commonRatioWithAnotherMeasure(measure2)).isBetween(0.333, 0.334);
+        assertThat(measure2.commonRatioWithAnotherMeasure(measure1)).isBetween(0.333, 0.334);
     }
 
 
@@ -22,8 +22,8 @@ public class MeasureTest {
         final Measure measure1 = Measure.withMin(0).andMax(5);
         final Measure measure2 = Measure.withMin(6).andMax(8);
 
-        final double commonRatio = measure1.commonRatioWithAnotherMeasure(measure2);
-        assertThat(commonRatio).isEqualTo(0);
+        assertThat(measure1.commonRatioWithAnotherMeasure(measure2)).isEqualTo(0);
+        assertThat(measure2.commonRatioWithAnotherMeasure(measure1)).isEqualTo(0);
     }
 
 }
